4335b840abd0516823730d6d266f8532590f9daa
Boogiee07 <152927777+Boogiee07@users.noreply.github.com>
2025-11-23 18:07:10 +0800

    Delete netlify/functions directory

--- netlify/functions/receive-data.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  if (event.httpMethod === 'POST') {
-    const data = JSON.parse(event.body || "{}");
-    return { statusCode:200, body: JSON.stringify({ received:true, data })};
-  }
-  return { statusCode:400, body:"Only POST allowed" };
-};
